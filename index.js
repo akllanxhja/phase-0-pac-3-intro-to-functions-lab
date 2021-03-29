15e47f06c2704f2bc870180e7e896b4f7615ce0c
AlexanderKllanxhja <akllanxhja@gmail.com>
2021-03-29 11:17:45 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase("hello"));
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase("HELLO"));
+}
+
+function sayHiToGrandma(string) {
+    if (string === string.toLowerCase()) {
+        string = "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+        string = "YES INDEED!";
+    } else {
+        string = "I love you, too.";
+    }
+    return string;
+}
+
+
+
